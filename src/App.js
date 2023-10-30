4c8ed360410fdd770429ed4287e46a6186b4215a
theartdr <129827570+theartdr@users.noreply.github.com>
2023-10-30 17:30:33 +0900

    add Route path

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
 import StampDetail from './Pages/StampDetail';
+import Join from './Pages/Join';
+import Login from './Pages/Login';
+import FindPassword from './Pages/FindPassword';
+import EditUserInfo from './Pages/EditUserInfo';
+
 
 function App() {
   return (
@@ -8,6 +13,10 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/stampDetail' element={<StampDetail />} />
+        <Route path='/join' element={<Join />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/findPassword' element={<FindPassword />} />
+        <Route path='/editUserInfo' element={<EditUserInfo />} />
       </Routes>
     </div>
   );
