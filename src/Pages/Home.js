4ecb2d70e78ffa98f38feabf743a20e5276c8900
songyunjeong <yungunaa@gmail.com>
2023-11-07 17:28:53 +0900

    feat: 댓글 api 연동 완료

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -65,7 +65,7 @@ const Home = () => {
                 id={book.stampbookId}
                 nickname={book.nickname}
                 description={book.description}
-                regdate={book.stampbookRegdate}
+                stampbookRegdate={book.stampbookRegdate}
                 likeCount={book.likeCount}
                 title={book.title}
                 key={i}
